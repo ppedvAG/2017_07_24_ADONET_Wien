9792ea52a0d01e39b4c8cb54858924ab74df86d6
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-07-24 10:04:53 +0200

    Add project files.

---
+++ HalloDatenbank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HalloDatenbank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
